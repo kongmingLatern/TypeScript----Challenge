fa60329a018ef79d75121d17f0e4a0b26e5e96c6
施颖杰 <1577310596@qq.com>
2022-04-20 10:27:41 +0800

    update: finish medium CamelCase

--- ts-challenge/medium/610-medium-camelcase/template.ts
+++ ts-challenge/medium/610-medium-camelcase/template.ts
@@ -1,1 +1,25 @@
-type CamelCase1<S extends string> = S extends `${infer F}-${infer S}${infer R}` ? S extends '-' | Uppercase<S> ? `${F}-${CamelCase<`${S}${R}`>}` : `${F}${Uppercase<S>}${CamelCase<R>}` : S;
+// type CamelCase1<S> =
+//   S extends `${infer First}-${infer Capital}${infer Rest}`
+//   ? Capital extends Capitalize<Capital>
+//   ? `${First}-${CamelCase<`${Capital}${Rest}`>}`
+//   : `${First}${Capitalize<Capital>}${CamelCase<Rest>}`
+//   : S;
+
+type CamelCase1<S extends string> =
+  S extends `${infer F}-${infer Capital}${infer Rest}`
+  ? Capital extends '-'
+  ? `${F}-${CamelCase1<`-${Rest}`>}`
+  : Capital extends Capitalize<Capital>
+  ? `${F}-${Capital}${CamelCase1<Rest>}`
+  : `${F}${Capitalize<Capital>}${CamelCase1<Rest>}`
+  : S
+
+type r11 = CamelCase1<'foo--bar----baz'>
+
+// type CamelCase1<S> =
+//   S extends `${infer First}-${infer Capital}${infer Rest}`
+//   ? Capital extends Capitalize<Capital>
+//   ? `${First}-${CamelCase<`${Capital}${Rest}`>}`
+//   : `${First}${Capitalize<Capital>}${CamelCase<Rest>}`
+// : S;
+
