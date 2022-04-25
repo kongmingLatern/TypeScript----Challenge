fb7fef82401f83cb696bf9bdbb6dbe28df3eadb5
施颖杰 <1577310596@qq.com>
2022-04-25 21:03:45 +0800

    update: finish medium PartialByKeys

---
+++ ts-challenge/medium/2757-medium-partialbykeys/template.ts
@@ -0,0 +1,14 @@
+
+type MergeType<O> = {
+  [P in keyof O]: O[P]
+}
+type PartialByKeys<T, K = keyof T> =
+  MergeType<
+    {
+      [P in keyof T as P extends K ? P : never]?: T[P]
+    }
+    &
+    {
+      [P in keyof T as P extends K ? never : P]: T[P]
+    }
+  >;
