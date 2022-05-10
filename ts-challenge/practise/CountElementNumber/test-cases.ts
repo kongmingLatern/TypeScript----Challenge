158884120a679b16a3ae537bc36a44eb38fad64a
施颖杰 <1577310596@qq.com>
2022-05-10 22:36:13 +0800

    update: finish custome CountElementNumberToObject

--- ts-challenge/practise/CountElementNumber/test-cases.ts
+++
@@ -1,9 +0,0 @@
-import { Equal, Expect } from "@type-challenges/utils";
-
-type cases = [
-  Expect<Equal<CountElementNumber<[1, 2, 3, 4, 4], 4>, 2>>,
-  Expect<Equal<CountElementNumber<[1, 2, 2, 3, 4, 5, 6], 5>, 1>>,
-  Expect<Equal<CountElementNumber<[1, 2, 2, 3, 4, 5, 6], 0>, 0>>,
-]
-// @ts-expect-error
-type error = CountElementNumber<[1, 2, 3], [1]>
