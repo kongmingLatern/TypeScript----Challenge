50bc2416724a246f96ceaa1ea42be5f6401a53bf
施颖杰 <1577310596@qq.com>
2022-05-11 22:00:33 +0800

    feat: add some cases

--- ts-challenge/practise/CountElementNumberToObject/template.ts
+++ ts-challenge/practise/CountElementNumberToObject/template.ts
@@ -4,9 +4,9 @@ type CountElementNumberToObject<
     [P in Flatten<T>[number]]: CountElementNumber<Flatten<T>, P>
   }
 
-type o = CountElementNumberToObject<[1, 2, 3, 4, 5]>
-type ppp = CountElementNumberToObject<[1, 2, 3, 4, 5, [1, 2, 3, 8], [[[[5]]]]]>
-type o = CountElementNumberToObject<[1, 2, 3, 4, 5]>
+type test1 = CountElementNumberToObject<[1, 2, 3, 4, 5]>
+type test2 = CountElementNumberToObject<[1, 2, 3, 4, 5, [1, 2, 3, 8], [[[[5]]]]]>
+type test3 = CountElementNumberToObject<[1, 2, 3, 4, 5]>
 
 
 type a = Flatten<[1, 2, 3, 4, 5, [1, 2, 3, 8]]>
