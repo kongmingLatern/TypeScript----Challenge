e3ee2c17b4140ca2619165474f94af2e79463066
施颖杰 <1577310596@qq.com>
2022-05-12 22:11:26 +0800

    update: finish medium AllCombinations

--- ts-challenge/medium/5310-medium-join/test-cases.ts
+++ ts-challenge/medium/5310-medium-join/test-cases.ts
@@ -1,4 +1,4 @@
-import { Equal, Expect, ExpectFalse, NotEqual } from '@type-challenges/utils'
+import { Equal, Expect } from '@type-challenges/utils'
 
 type cases = [
   Expect<Equal<Join<["a", "p", "p", "l", "e"], "-">, "a-p-p-l-e">>,
---
+++ ts-challenge/medium/AllCombinations/template.ts
@@ -0,0 +1,37 @@
+// type StringToUnion<S extends string> = S extends `${infer L}${infer R}` ? L | StringToUnion<R> : S;
+// type Combination<A extends string, B extends string> =
+//   | A
+//   | B
+//   | `${A}${B}`
+//   | `${B}${A}`;
+
+// type AllCombinations<A extends string, B extends string = A> =
+//   '' |
+//   A extends A
+//   ? Combination<A, AllCombinations<Exclude<B, A>>>
+//   : never;
+// type p = AllCombinations<'A' | 'B' | 'C'>
+
+
+
+
+// 思路
+// 1.将字符串转化为联合类型
+type StringToUnion1<S extends string> = S extends `${infer L}${infer R}` ? L | StringToUnion1<R> : S;
+// 2.联合类型两两合并
+type Combination1<A extends string, B extends string> =
+  | ''
+  | A
+  | B
+  | `${A}${B}`
+  | `${B}${A}`;
+// 3 etc.
+type test1 = Combination1<'A' | 'B', 'C' | 'D'>
+//4.联合类型的合并，利用联合类型默认可拆解
+type UnionCombination<A extends string, B extends string = A> =
+  A extends '' ? '' :
+  A extends B
+  ? Combination1<A, UnionCombination<Exclude<B, A>>>
+  : never;
+//5. 字符串合并
+type AllCombinations<S extends string> = UnionCombination<StringToUnion1<S>>
