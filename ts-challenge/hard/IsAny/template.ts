c556d381f860a2d09a33a21e7c2e2610a2639a82
施颖杰 <1577310596@qq.com>
2022-05-28 14:01:21 +0800

    feat: add some instruction

--- ts-challenge/hard/IsAny/template.ts
+++ ts-challenge/hard/IsAny/template.ts
@@ -7,8 +7,8 @@ type IsAny<T extends string | undefined | unknown> = boolean extends
   ? true
   : false;
 
-type test1 = boolean extends true ? true : false
-type test2 = boolean extends false ? true : false
-type test3 = boolean extends any ? true : false
+type test1 = boolean extends true ? true : false // false
+type test2 = boolean extends false ? true : false // false  
+type test3 = boolean extends any ? true : false  // true 
 // any extends 任何东西 都是 后面 两个的联合类型 (true | false) => false
 type test4 = any extends string ? true : false
