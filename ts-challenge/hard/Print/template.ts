570a25ef11b56256591eaccc4fb42cc36dc9e0c0
施颖杰 <1577310596@qq.com>
2022-06-01 14:46:16 +0800

    chore: finish hard Format

---
+++ ts-challenge/hard/Print/template.ts
@@ -0,0 +1,8 @@
+type Format<T extends string> =
+  T extends `${infer First}%${infer F & ("s" | "d")}${infer Rest}`
+  ? F extends 's'
+  ? (s1: string) => Format<`${First}${Rest}`>
+  : (d1: number) => Format<`${First}${Rest}`>
+  : string
+
+type Interaction<T> = 's' & ("s" | "d")
