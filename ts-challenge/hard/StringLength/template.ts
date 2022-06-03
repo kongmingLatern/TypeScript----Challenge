d7857873dd0e9a381e6d317b4a5a2f368a40b7e5
施颖杰 <1577310596@qq.com>
2022-06-03 20:14:17 +0800

    chore: finish hard LengthOfString 2

---
+++ ts-challenge/hard/StringLength/template.ts
@@ -0,0 +1,11 @@
+type StringOfLength<
+  T extends string,
+  S extends string[] = []
+  > =
+  T extends ''
+  ? S["length"]
+  : T extends `${infer F}${infer Rest}`
+  ? StringOfLength<Rest, [...S, F]>
+  : never
+
+type testStringLength = StringOfLength<'12345678901234'>
