2ac6b62add685b80901bba77dc4923013a31ca63
施颖杰 <1577310596@qq.com>
2022-06-04 20:35:53 +0800

    chore: finish hard UnionToTuple

--- ts-challenge/hard/UnionToTuple/template.ts
+++ ts-challenge/hard/UnionToTuple/template.ts
@@ -1,10 +1,20 @@
+type UnionToIntersectionFn<U> = (
+  U extends unknown ? (K: () => U) => void : never
+) extends (K: infer I) => void ? I : never;
+
+type GetUnionLast<U> = UnionToIntersectionFn<U> extends () => infer I
+  ? I : never;
+
+type Prepend<Tuple extends unknown[], First> = [First, ...Tuple];
+
 type UnionToTuple<
-  T extends any,
-  S extends unknown[] = []
-  > =
-  T extends T
-  ? [...S, T]
-  : S
+  Union,
+  T extends unknown[] = [],
+  Last = GetUnionLast<Union>
+  > = [Union] extends [never]
+  ? T
+  : UnionToTuple<Exclude<Union, Last>, Prepend<T, Last>>;
+
 type ExtractValuesOfTuple<T extends any[]> = T[keyof T & number]
 
 type testUnion = UnionToTuple<'a' | 'b'>['length']
