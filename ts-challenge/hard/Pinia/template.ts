55047920ae857bf42559fd862385d76f3bc6a61c
施颖杰 <1577310596@qq.com>
2022-06-07 10:18:48 +0800

    chore: finish hard Pinia

--- ts-challenge/hard/Pinia/template.ts
+++ ts-challenge/hard/Pinia/template.ts
@@ -1,1 +1,28 @@
-declare function defineStore(store: unknown): unknown
+// declare function defineStore<I, S, G, A>(store: {
+//   id: I,
+//   state: () => S,
+//   getters: S & ThisType<I & S & (G extends Record<string, () => infer ReturnType> ? Record<string, ReturnType> : G)>
+//   actions: () => A,
+// }): Record<string, S & I & G & A> 
+// declare function defineStore<S, G, A>(store: {
+//   id: string
+//   state: () => S
+//   getters: G & ThisType<S & { [K in keyof G]: G[K] extends () => infer TReturn ? TReturn : G[K] }>
+//   actions: A & ThisType<S & G & A>
+// }): S & A & { [K in keyof G]: G[K] extends () => infer TReturn ? TReturn : G[K] }
+declare function defineStore<
+  State,
+  Getters,
+  Actions,
+  DefinedGetters extends Readonly<{
+    [Key in keyof Getters]: Getters[Key] extends () => infer Value
+    ? Value
+    : never
+  }>,
+  DefinedStore extends State & DefinedGetters & Actions,
+  >(store: {
+    id: string,
+    state: () => State,
+    getters: Getters & ThisType<Readonly<DefinedStore>>,
+    actions: Actions & ThisType<DefinedStore>,
+  }): DefinedStore
