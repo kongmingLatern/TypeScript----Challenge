766191badb4faa23b07451c8a2456dbac5c62b39
施颖杰 <1577310596@qq.com>
2022-06-10 09:16:53 +0800

    chore: finish hard IsRequiredKey

---
+++ ts-challenge/hard/IsRequireKey/template.ts
@@ -0,0 +1,18 @@
+type GetKey<T extends Record<string, any>> = {
+  [P in keyof T]-?: T[P]
+}
+
+type getRequireKeys<
+  T extends Record<string, any>,
+  K extends T = GetKey<T>
+  > = {
+    [P in keyof T as T[P] extends K[P] ? P : never]: T[P]
+  }
+
+type IsRequiredKey<
+  T extends Record<string, any>,
+  K extends string
+  > =
+  [K] extends [keyof getRequireKeys<T>]
+  ? true
+  : false
