cf2f95fdcbc7dff39afec75de58e47d899846b64
施颖杰 <1577310596@qq.com>
2022-06-11 12:45:25 +0800

    enhance: add a test-case about StringToNumber

--- ts-challenge/hard/StringToNumebr/template.ts
+++ ts-challenge/hard/StringToNumebr/template.ts
@@ -1,4 +1,20 @@
+type NumberA = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9
+
+type IsNumber<T extends string, K extends boolean = true> =
+  T extends ''
+  ? K
+  : T extends `${infer F}${infer Rest}`
+  ? F extends `${NumberA}`
+  ? IsNumber<Rest, true>
+  : IsNumber<Rest, false>
+  : false
+
+type testNumber = IsNumber<'11$%'>
+
+
 type ToNumber<T extends string, S extends unknown[] = []> =
-  T extends `${S["length"]}`
+  IsNumber<T> extends true
+  ? T extends `${S["length"]}`
   ? S["length"]
   : ToNumber<T, [...S, any]>
+  : never
