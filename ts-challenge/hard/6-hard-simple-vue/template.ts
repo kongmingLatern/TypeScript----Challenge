167dc4443930dd05d86d422b1ebaa35f89ca4639
施颖杰 <1577310596@qq.com>
2022-06-15 14:54:59 +0800

    fix: fix some more spaces

--- ts-challenge/hard/6-hard-simple-vue/template.ts
+++ ts-challenge/hard/6-hard-simple-vue/template.ts
@@ -3,3 +3,4 @@ declare function SimpleVue<D, C, M>(options: {
   computed: C & ThisType<D & C>,
   methods: M & ThisType<D & M & (C extends Record<string, () => infer ReturnType> ? Record<string, ReturnType> : C)>
 }): any
+
