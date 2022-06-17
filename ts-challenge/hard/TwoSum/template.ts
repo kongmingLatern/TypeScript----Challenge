9da1d2a897937ba37f09400f81b665b19016ef75
施颖杰 <1577310596@qq.com>
2022-06-17 14:56:53 +0800

    chore: finish hard TwoSum

---
+++ ts-challenge/hard/TwoSum/template.ts
@@ -0,0 +1,25 @@
+type ToArray<
+  T extends number,
+  A extends number[] = []
+  > = A['length'] extends T
+  ? A
+  : ToArray<T, [...A, 1]>;
+
+type diff<
+  S extends number,
+  U extends number
+  > = ToArray<S> extends [...ToArray<U>, ...infer R]
+  ? R['length']
+  : -1;
+
+type TwoSum<
+  T extends number[],
+  U extends number
+  > =
+  T extends [T[0], ...infer R]
+  ? diff<U, T[0]> extends R[number]
+  ? true
+  : R extends number[]
+  ? TwoSum<R, U>
+  : false
+  : false
