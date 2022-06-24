34fa09c39a96f3b0c19f43fbf990d6c1910530c7
施颖杰 <1577310596@qq.com>
2022-06-24 22:03:25 +0800

    chore: finish extreme GetReadonlyKeys

---
+++ ts-challenge/Extreme/GetReadonlyKeys/template.ts
@@ -0,0 +1,15 @@
+type GetReadonlyKeys<T> = keyof {
+  [
+  K in keyof T as (
+    <P>() => P extends ({ [P in K]: T[P] }) ? true : never
+  ) extends (
+    <P>() => P extends ({ readonly [P in K]: T[P] }) ? true : never
+  )
+  ? K
+  : never
+  ]: never
+};
+
+// type GetReadonlyKeys<T> = keyof {
+//   [k in keyof T as Equal<{[x in k]:T[x]},{readonly [x in k]:T[k]}> extends true ? k : never] : T[k]
+// }
