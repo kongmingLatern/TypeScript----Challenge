359db284e5a0bdd541708413c75a640f0758509d
施颖杰 <1577310596@qq.com>
2022-06-28 17:36:43 +0800

    fix: fix some typo

--- ts-challenge/Extreme/GetReadonlyKeys/template.ts
+++ ts-challenge/Extreme/GetReadonlyKeys/template.ts
@@ -10,6 +10,7 @@ type GetReadonlyKeys<T> = keyof {
   ]: never
 };
 
+
 // type GetReadonlyKeys<T> = keyof {
 //   [k in keyof T as Equal<{[x in k]:T[x]},{readonly [x in k]:T[k]}> extends true ? k : never] : T[k]
 // }
